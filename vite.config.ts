1a27f9c2bc1d8e74598195e174719a0aeea6418b
Zhou Peng <1912306901@qq.com>
2023-10-12 14:25:05 +0800

    细节调整

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import uni from '@dcloudio/vite-plugin-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // 开发阶段启用源码映射
+    sourcemap: process.env.NODE_ENV === 'development'
+  },
   plugins: [uni(), UnoCSS()]
 })
