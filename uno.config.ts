19afa19e1698af2904a2c537663a2693eb013513
Zhou Peng <1912306901@qq.com>
2023-10-12 16:32:03 +0800

    添加unocss的字体图标之前

--- uno.config.ts
+++ uno.config.ts
@@ -2,16 +2,22 @@ import { defineConfig, presetIcons } from 'unocss'
 import presetWeapp from 'unocss-preset-weapp'
 import { extractorAttributify, transformerClass } from 'unocss-preset-weapp/transformer'
 
+// class前缀解决与其它原子化css产生冲突
+const prefix = 'u_'
+
 const { presetWeappAttributify, transformerAttributify } = extractorAttributify({
-  classPrefix: 'u_'
+  classPrefix: prefix
 })
 
 export default defineConfig({
   presets: [
     presetWeapp({
-      prefix: 'u_'
+      transform: true, // 转换微信 class
+      platform: 'uniapp', // 平台
+      whRpx: true, // 使用 rpx 为默认单位
+      prefix
     }),
-    presetWeappAttributify()
+    presetWeappAttributify() //vscode插件 autocomplete 提示
     // presetIcons({
     //   scale: 1.2,
     //   warn: true
@@ -22,5 +28,8 @@ export default defineConfig({
       u_center: 'u_flex u_justify-center u_items-center'
     }
   ],
-  transformers: [transformerAttributify(), transformerClass()]
+  transformers: [
+    transformerAttributify(), // 支持 attributify mode
+    transformerClass() // 转换转义类名
+  ]
 })
